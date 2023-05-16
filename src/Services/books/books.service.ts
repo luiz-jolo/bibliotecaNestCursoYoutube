d8bcbadbfa7c28c67d22f39332f15bf21a00ad25
devlujo <devlujoworks@gmail.com>
2023-05-15 22:27:51 -0400

    Including the route Get for getting all books

--- src/Services/books/books.service.ts
+++ src/Services/books/books.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { BookDTO } from 'src/DTO/books.dto';
 import { BookRepository } from 'src/mongo/Repositories/book.repository';
 import { Book } from 'src/mongo/Interfaces/book.interface';
@@ -10,9 +10,17 @@ export class BooksService {
         private readonly bookRepository : BookRepository
     ){}
 
-
+    async getAllBooks(): Promise<Book[]>{
+        const allBooks = await this.bookRepository.getAllBooks();
+        if(!allBooks.length){
+            throw new BadRequestException('There are no books registered yet');
+        }
+        return allBooks;
+    }
+    
     async saveBook(newBook: BookDTO) : Promise<Book> {
         return await this.bookRepository.saveBook(newBook);
     }
 
+    
 }
